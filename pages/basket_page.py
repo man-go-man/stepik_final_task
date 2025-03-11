b63f8651693ff0c66bc9a2629b827a5390f0e969
Dmitriy Gorbunov <dgorbunovm@mail.ru>
2025-03-11 12:25:44 +0300

    добавил методы и проверки, что корзина пуста, при переходе в неё с главной и продуктовой страниц

---
+++ pages/basket_page.py
@@ -0,0 +1,10 @@
+from pages.base_page import BasePage
+from pages.locators import BasketPageLocators
+
+
+class BasketPage(BasePage):
+    def should_not_see_any_items_in_basket(self):
+        assert self.is_not_element_present(*BasketPageLocators.ITEM_PRESENCE_ELEMENT), "Items in basket are presented"
+
+    def should_see_empty_basket_text(self):
+        assert self.is_element_present(*BasketPageLocators.BASKET_EMPTY_TEXT), "Empty basket text is not presented"
